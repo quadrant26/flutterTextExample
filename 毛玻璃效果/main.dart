c31a12c8a3ececdd5119a34cf5661985ba57f83b
quadrant26 <quadrant26@163.com>
2019-05-30 17:58:28 +0800

    毛玻璃效果

---
+++ 毛玻璃效果/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import './keep_alive_demo.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue
+      ),
+      home: keepAliveDemo(),
+    );
+  }
+}
+
+class keepAliveDemo extends StatefulWidget {
+  @override
+  _keepAliveDemoState createState() => _keepAliveDemoState();
+}
+
+class _keepAliveDemoState extends State<keepAliveDemo> with SingleTickerProviderStateMixin {
+
+  TabController _controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Keep Alive Demo'),
+        bottom: TabBar(
+          controller: _controller,
+          tabs: [
+            Tab(icon: Icon(Icons.directions_car)),
+            Tab(icon: Icon(Icons.directions_transit)),
+            Tab(icon: Icon(Icons.directions_bike)),
+          ]
+        )
+      ),
+      body: TabBarView(
+        controller: _controller,
+        children: <Widget>[
+          MyHomePage(),
+          MyHomePage(),
+          MyHomePage(),
+        ],
+      ),
+    );
+  }
+}
