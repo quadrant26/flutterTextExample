5c0fdb9d8c88e57f961d80f99c5b83ef94595c02
quadrant26 <quadrant26@163.com>
2019-05-30 17:57:27 +0800

    底部导航栏

---
+++ 底部导航栏/pages/airplay_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Airplay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Airplay')),
+      body: Center(child: Text('AIRPLAY'),)
+    );
+  }
+}
